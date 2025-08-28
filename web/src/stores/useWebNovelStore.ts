132a83b1834b0b0c1e759cd0c4ced41b32e762f9
FishHawk <whang23@outlook.com>
2025-08-29 02:32:11 +0800

    chore(web): 重构NovelStore使用组合式api

--- web/src/stores/useWebNovelStore.ts
+++ web/src/stores/useWebNovelStore.ts
@@ -2,35 +2,35 @@ import { WebNovelApi } from '@/data';
 import { WebNovelDto } from '@/model/WebNovel';
 import { Result, runCatching } from '@/util/result';
 
-type WebNovelStore = {
-  novelResult: Result<WebNovelDto> | undefined;
-};
-
 export const useWebNovelStore = (providerId: string, novelId: string) => {
-  return defineStore(`web-novel/${providerId}/${novelId}`, {
-    state: () =>
-      <WebNovelStore>{
-        novelResult: undefined,
-      },
-    actions: {
-      async loadNovel(force = false) {
-        if (!force && this.novelResult?.ok) {
-          return this.novelResult;
-        }
+  return defineStore(`web-novel/${providerId}/${novelId}`, () => {
+    const novelResult = ref<Result<WebNovelDto>>();
 
-        this.novelResult = undefined;
-        const result = await runCatching(
-          WebNovelApi.getNovel(providerId, novelId),
-        );
-        this.novelResult = result;
+    async function loadNovel(force = false) {
+      if (!force && novelResult.value?.ok) {
+        return novelResult.value;
+      }
 
-        return this.novelResult;
-      },
+      novelResult.value = undefined;
+      const result = await runCatching(
+        WebNovelApi.getNovel(providerId, novelId),
+      );
+      novelResult.value = result;
 
-      async updateNovel(json: Parameters<typeof WebNovelApi.updateNovel>[2]) {
-        await WebNovelApi.updateNovel(providerId, novelId, json);
-        this.loadNovel(true);
-      },
-    },
+      return result;
+    }
+
+    async function updateNovel(
+      json: Parameters<typeof WebNovelApi.updateNovel>[2],
+    ) {
+      await WebNovelApi.updateNovel(providerId, novelId, json);
+      loadNovel(true);
+    }
+
+    return {
+      novelResult,
+      loadNovel,
+      updateNovel,
+    };
   })();
 };
--- web/src/stores/useWenkuNovelStore.ts
+++ web/src/stores/useWenkuNovelStore.ts
@@ -2,91 +2,91 @@ import { WenkuNovelApi } from '@/data';
 import { WenkuNovelDto } from '@/model/WenkuNovel';
 import { Result, runCatching } from '@/util/result';
 
-type WenkuNovelStore = {
-  novelResult: Result<WenkuNovelDto> | undefined;
-};
-
 export const useWenkuNovelStore = (novelId: string) => {
-  return defineStore(`wenku-novel/${novelId}`, {
-    state: () =>
-      <WenkuNovelStore>{
-        novelResult: undefined,
-      },
-    actions: {
-      async loadNovel(force = false) {
-        if (!force && this.novelResult?.ok) {
-          return this.novelResult;
-        }
+  return defineStore(`wenku-novel/${novelId}`, () => {
+    const novelResult = ref<Result<WenkuNovelDto>>();
 
-        this.novelResult = undefined;
-        const result = await runCatching(WenkuNovelApi.getNovel(novelId));
-        if (result.ok) {
-          result.value.volumeZh = result.value.volumeZh.sort((a, b) =>
-            a.localeCompare(b),
-          );
-          result.value.volumeJp = result.value.volumeJp.sort((a, b) =>
-            a.volumeId.localeCompare(b.volumeId),
-          );
-        }
-        this.novelResult = result;
+    async function loadNovel(force = false) {
+      if (!force && novelResult.value?.ok) {
+        return novelResult.value;
+      }
 
-        return this.novelResult;
-      },
-
-      async updateNovel(json: Parameters<typeof WenkuNovelApi.updateNovel>[1]) {
-        await WenkuNovelApi.updateNovel(novelId, json);
-        this.loadNovel(true);
-      },
-
-      async createVolume(
-        volumeId: string,
-        type: 'jp' | 'zh',
-        file: File,
-        onProgress: (p: number) => void,
-      ) {
-        const total = await WenkuNovelApi.createVolume(
-          novelId,
-          volumeId,
-          type,
-          file,
-          onProgress,
+      novelResult.value = undefined;
+      const result = await runCatching(WenkuNovelApi.getNovel(novelId));
+      if (result.ok) {
+        result.value.volumeZh = result.value.volumeZh.sort((a, b) =>
+          a.localeCompare(b),
         );
+        result.value.volumeJp = result.value.volumeJp.sort((a, b) =>
+          a.volumeId.localeCompare(b.volumeId),
+        );
+      }
+      novelResult.value = result;
 
-        if (this.novelResult?.ok) {
-          if (type === 'jp') {
-            this.novelResult.value.volumeJp.push({
-              volumeId,
-              total: Number(total),
-              baidu: 0,
-              youdao: 0,
-              gpt: 0,
-              sakura: 0,
-            });
-            this.novelResult.value.volumeJp =
-              this.novelResult.value.volumeJp.sort((a, b) =>
-                a.volumeId.localeCompare(b.volumeId),
-              );
-          } else {
-            this.novelResult.value.volumeZh.push(volumeId);
-            this.novelResult.value.volumeZh =
-              this.novelResult.value.volumeZh.sort((a, b) =>
-                a.localeCompare(b),
-              );
-          }
-        }
-      },
+      return result;
+    }
 
-      async deleteVolume(volumeId: string) {
-        await WenkuNovelApi.deleteVolume(novelId, volumeId);
-        if (this.novelResult?.ok) {
-          this.novelResult.value.volumeJp =
-            this.novelResult.value.volumeJp.filter(
-              (it) => it.volumeId !== volumeId,
+    async function updateNovel(
+      json: Parameters<typeof WenkuNovelApi.updateNovel>[1],
+    ) {
+      await WenkuNovelApi.updateNovel(novelId, json);
+      loadNovel(true);
+    }
+
+    async function createVolume(
+      volumeId: string,
+      type: 'jp' | 'zh',
+      file: File,
+      onProgress: (p: number) => void,
+    ) {
+      const total = await WenkuNovelApi.createVolume(
+        novelId,
+        volumeId,
+        type,
+        file,
+        onProgress,
+      );
+
+      if (novelResult.value?.ok) {
+        if (type === 'jp') {
+          novelResult.value.value.volumeJp.push({
+            volumeId,
+            total: Number(total),
+            baidu: 0,
+            youdao: 0,
+            gpt: 0,
+            sakura: 0,
+          });
+          novelResult.value.value.volumeJp =
+            novelResult.value.value.volumeJp.sort((a, b) =>
+              a.volumeId.localeCompare(b.volumeId),
             );
-          this.novelResult.value.volumeZh =
-            this.novelResult.value.volumeZh.filter((it) => it !== volumeId);
+        } else {
+          novelResult.value.value.volumeZh.push(volumeId);
+          novelResult.value.value.volumeZh =
+            novelResult.value.value.volumeZh.sort((a, b) => a.localeCompare(b));
         }
-      },
-    },
+      }
+    }
+
+    async function deleteVolume(volumeId: string) {
+      await WenkuNovelApi.deleteVolume(novelId, volumeId);
+      if (novelResult.value?.ok) {
+        novelResult.value.value.volumeJp =
+          novelResult.value.value.volumeJp.filter(
+            (it) => it.volumeId !== volumeId,
+          );
+        novelResult.value.value.volumeZh =
+          novelResult.value.value.volumeZh.filter((it) => it !== volumeId);
+      }
+    }
+
+    return {
+      novelResult,
+      loadNovel,
+      updateNovel,
+      createVolume,
+      deleteVolume,
+    };
   })();
 };
