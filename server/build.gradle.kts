be1d4bd0d7c01e97a9de3ea4cc6ff5b08dc7d7c2
KurikoMoe <kurikomoe@gmail.com>
2025-09-19 23:45:25 +0800

    add(tests): add test files for epub zip test
    add test files for epub zip test
    update kotest version to avoid test failure due to framework

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 
     implementation("io.github.crackthecodeabhi:kreds:0.9.1")
 
-    val kotestVersion = "5.5.4"
+    val kotestVersion = "5.9.1"
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-koin:1.1.0")
