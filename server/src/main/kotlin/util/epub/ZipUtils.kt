53dec2102ae46d800a362736ab323fdf1ed2c558
FishHawk <whang23@outlook.com>
2025-09-22 00:05:35 +0800

    修复unzipByJDK

--- server/src/main/kotlin/util/epub/ZipUtils.kt
+++ server/src/main/kotlin/util/epub/ZipUtils.kt
@@ -2,18 +2,13 @@ package util.epub
 
 import com.google.common.jimfs.Configuration
 import com.google.common.jimfs.Jimfs
-import net.lingala.zip4j.io.inputstream.ZipInputStream
 import org.slf4j.LoggerFactory
 import java.nio.file.FileSystem
+import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
 
 
-/**
- * 修正 zip 内路径访问。
- *
- * @param: zip 内文件路径
- */
 fun FileSystem.zipGetPath(path: String): Path {
     return if (!path.startsWith("/")) {
         getPath(Path.of("/").resolve(path).toString())
@@ -25,37 +20,27 @@ fun FileSystem.zipGetPath(path: String): Path {
 object ZipUtils {
     val logger = LoggerFactory.getLogger(ZipUtils::class.java)
 
-    // FIXME(kuriko): 请考虑把所有常量找个地方放一起。
     const val MAX_ALLOWED_EPUB_SIZE = 50 * 1024 * 1024  // 50MiB
 
     /**
      * @param zipPath epub/zip 文件路径
      */
     inline fun use(zipPath: Path, block: (FileSystem) -> Unit) {
-        val jimfs = Jimfs.newFileSystem(Configuration.unix())
-
-        jimfs.use { fs ->
-            val unzipMethods = listOf(ZipUtils::unzipByJDK, ZipUtils::unzipByZip4J)
-            for (method in unzipMethods) {
-                try {
-                    method(zipPath, fs)
-                    block(fs)
-                    return@use
-                } catch (e: Exception) {
-                    logger.warn("Failed to unzip $zipPath with ${method.name}.", e)
-                }
+        val unzipMethods = listOf(ZipUtils::unzipByJDK, ZipUtils::unzipByZip4J)
+        for (method in unzipMethods) {
+            try {
+                method(zipPath).use(block)
+                return
+            } catch (e: Exception) {
+                logger.warn("Failed to unzip $zipPath with ${method.name}.", e)
             }
-            throw Exception("Failed to unzip $zipPath.")
         }
+        throw Exception("Failed to unzip $zipPath.")
     }
 
     // Consider moving the unzipByXXX methods to a separate class and make them reusable.
-    /**
-     * Unzip via zip4j method
-     * @param zipPath zip file path
-     *
-     */
-    fun unzipByZip4J(zipPath: Path, fs: FileSystem) {
+    fun unzipByZip4J(zipPath: Path): FileSystem {
+        val fs = Jimfs.newFileSystem(Configuration.unix())
         var decSize = 0
         Files.newInputStream(zipPath).use { inputStream ->
             net.lingala.zip4j.io.inputstream.ZipInputStream(inputStream).use { zipInputStream ->
@@ -83,50 +68,11 @@ object ZipUtils {
                 }
             }
         }
+        return fs
     }
 
-    /**
-     * Unzip via java native zip method.
-     * @param zipPath zip file path
-     */
-    fun unzipByJDK(zipPath: Path, fs: FileSystem) {
-        var decSize = 0
-        Files.newInputStream(zipPath).use { inputStream ->
-            java.util.zip.ZipInputStream(inputStream).use { zis ->
-                generateSequence { zis.nextEntry }.forEach { entry ->
-                    // TODO(kuriko): check invalid zip entry name.
-                    val entryFile = Path.of("/").resolve(entry.name)
-                    val entryPath = fs.getPath(entryFile.toString())
-                    if (entry.isDirectory) {
-                        Files.createDirectories(entryPath)
-                    } else {
-                        // Make sure the parent exists.
-                        Files.createDirectories(entryPath.parent ?: fs.getPath("/"))
-                        Files.newOutputStream(entryPath).use { outputStream ->
-                            val buffer = ByteArray(4096)
-                            var len: Int
-                            while (zis.read(buffer).also { len = it } > 0) {
-                                outputStream.write(buffer, 0, len)
-                                decSize += len
-                                if (decSize > MAX_ALLOWED_EPUB_SIZE) {
-                                    throw IllegalStateException("Decompressed epub size exceeds the limit.")
-                                }
-                            }
-                        }
-                    }
-
-                }
-
-            }
-        }
-    }
-
-    /**
-     * Unzip via apache commons compress method.
-     * @param zipPath zip file path
-     */
-    private fun unzipByApache(zipPath: Path, fs: FileSystem) {
-        throw NotImplementedError("Unzip via apache commons compress method is not implemented yet.")
+    fun unzipByJDK(zipPath: Path): FileSystem {
+        return FileSystems.newFileSystem(zipPath)
     }
 }
 
--- server/src/test/kotlin/util/ZipUtils.kt
+++ server/src/test/kotlin/util/ZipUtils.kt
@@ -3,26 +3,16 @@ package util
 import io.kotest.core.spec.style.DescribeSpec
 import util.epub.ZipUtils
 import java.nio.file.Files
-import java.nio.file.Paths
+import kotlin.io.path.Path
+import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.name
 
 class EpubZipTest : DescribeSpec({
-    describe("processing all EPUB files in the 'resources/util' directory") {
-        val epubTestFiles = listOf("corrupted.01.epub")
-
-        val epubFiles = epubTestFiles
-            .mapNotNull { filename -> EpubZipTest::class.java.getResource("/util/$filename") }
-            .mapNotNull { file -> Paths.get(file.toURI()) }
-            .toList()
-
-        if (epubFiles.isEmpty()) {
-            it("missing epub test files") {
-                throw AssertionError("No EPUB test files found.")
-            }
-        }
-
-        epubFiles.forEach { epubPath ->
-            it("testing unpacking ${epubPath.name}") {
+    describe("ZipUtils") {
+        it("processing all EPUB files in the 'resources/util' directory") {
+            val epubFiles = Path("data/files-test").listDirectoryEntries()
+            epubFiles.forEach { epubPath ->
+                println(epubPath)
                 ZipUtils.use(epubPath) { fs ->
                     val root = fs.rootDirectories.first()
                     println("Checking epub: ${epubPath.name}")
