bd1c2aff41d99f31f3e9a236dc165032e9e7a664
KurikoMoe <kurikomoe@gmail.com>
2025-10-02 23:59:10 +0800

    use raw api

--- web/src/util/useAddon/index.ts
+++ web/src/util/useAddon/index.ts
@@ -1,5 +1,11 @@
-import { ClientMethods, SerializableResponse } from './client.types';
-import { Message, MSG_CRAWLER, MSG_RESPONSE, MSG_TYPE } from './msg';
+import ky_orig from 'ky';
+import {
+  serializeRequest,
+  type ClientMethods,
+  type SerializableResponse,
+} from './client.types';
+import type { Message, MSG_CRAWLER, MSG_RESPONSE } from './msg';
+import { MSG_TYPE } from './msg';
 
 class AddonCommunication {
   idx: number;
@@ -10,12 +16,28 @@ class AddonCommunication {
     this.idx = 1;
   }
 
-  wrapResponse(response: SerializableResponse): any {
-    return {
-      text: async () => response.body,
-      json: async () => JSON.parse(response.body),
-      ...response,
+  wrapResponse(resp: SerializableResponse): Response {
+    const init: ResponseInit = {
+      status: resp.status,
+      statusText: resp.statusText,
+      headers: resp.headers,
     };
+    const realResp = new Response(resp.body, init);
+    return realResp;
+  }
+
+  buildCrawlerMessage<P>(
+    cmd: string,
+    params: P,
+    base_url = '',
+    single = true,
+  ): MSG_CRAWLER {
+    const msg: MSG_CRAWLER = {
+      type: MSG_TYPE.CRAWLER_REQ,
+      id: (this.idx++).toString(),
+      payload: { base_url, single, cmd, params },
+    };
+    return msg;
   }
 
   public async sendMessage<T>(msg: Message): Promise<T> {
@@ -39,10 +61,36 @@ class AddonCommunication {
     });
   }
 
+  public async http_raw_fetch(
+    input: RequestInfo | URL,
+    requestInit?: RequestInit,
+  ): Promise<Response> {
+    let url: string;
+    if (input instanceof URL) {
+      input = input.toString();
+      url = input;
+    }
+    input = input as Request | string;
+    if (typeof input === 'string') {
+      url = input;
+    } else {
+      url = input.url;
+    }
+    type ParamType = Parameters<ClientMethods['http.raw']>[0];
+    const serInput = await serializeRequest(input);
+    const msg = this.buildCrawlerMessage<ParamType>(
+      'http.raw',
+      { input: serInput, requestInit },
+      url,
+    );
+    const resp: SerializableResponse = await this.sendMessage(msg);
+    return this.wrapResponse(resp);
+  }
+
   public async http_get(
     url: string,
     searchParams = {},
-    headers = {},
+    headers = [],
   ): Promise<Response> {
     type ParamType = Parameters<ClientMethods['http.get']>[0];
     const params: ParamType = {
@@ -51,15 +99,7 @@ class AddonCommunication {
       headers,
     };
 
-    const msg: MSG_CRAWLER = {
-      type: MSG_TYPE.CRAWLER_REQ,
-      payload: {
-        base_url: url,
-        single: true,
-        cmd: 'http.get',
-        params,
-      },
-    };
+    const msg = this.buildCrawlerMessage('http.get', params, url);
     const resp: SerializableResponse = await this.sendMessage(msg);
     return this.wrapResponse(resp);
   }
@@ -67,7 +107,7 @@ class AddonCommunication {
   public async http_post(
     url: string,
     data = {},
-    headers = {},
+    headers = [],
   ): Promise<Response> {
     type ParamType = Parameters<ClientMethods['http.postJson']>[0];
     const params: ParamType = {
@@ -75,16 +115,7 @@ class AddonCommunication {
       data,
       headers,
     };
-
-    const msg: MSG_CRAWLER = {
-      type: MSG_TYPE.CRAWLER_REQ,
-      payload: {
-        base_url: url,
-        single: true,
-        cmd: 'http.post',
-        params,
-      },
-    };
+    const msg = this.buildCrawlerMessage('http.postJson', params, url);
     const resp: SerializableResponse = await this.sendMessage(msg);
     return this.wrapResponse(resp);
   }
@@ -102,28 +133,17 @@ export interface Options {
 }
 
 export class AddonClient {
-  static addonID = 'heaclbjdecgjhkbeigpkgoboipadjalj';
+  static addonID = 'nanejbmiefglbjfibnabpedojhhajdck';
   private comm: AddonCommunication;
 
   constructor() {
     this.comm = new AddonCommunication(AddonClient.addonID);
   }
 
-  public async get(url: string, options?: Options): Promise<Response> {
-    console.log('get', url, options);
-    const final_url = new URL(url, options?.prefixUrl ?? '/');
-    const searchParams = options?.searchParams || {};
-    return await this.comm.http_get(final_url.toString(), searchParams);
-  }
-
-  public async postJson(url: string, options?: Options): Promise<Response> {
-    console.log('post', url, options);
-    const final_url = new URL(url, options?.prefixUrl ?? '/');
-    const json = options?.json || {};
-    return await this.comm.http_get(final_url.toString(), json, {
-      ...options?.headers,
-    });
+  async fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response> {
+    return await this.comm.http_raw_fetch(input, init);
   }
 }
 
 export const addon = new AddonClient();
+export const ky = ky_orig.create({ fetch: addon.fetch.bind(addon) });
