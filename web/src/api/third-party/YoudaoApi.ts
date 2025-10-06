1cab62d49f762c452e4597d0452fe8577bca7317
KurikoMoe <kurikomoe@gmail.com>
2025-10-07 02:18:15 +0800

    update misc

--- web/src/api/third-party/YoudaoApi.ts
+++ web/src/api/third-party/YoudaoApi.ts
@@ -3,10 +3,11 @@ import { Utf8 } from 'crypto-es/lib/core';
 import { MD5 } from 'crypto-es/lib/md5';
 import type { Options } from 'ky';
 // import ky from 'ky';
-import { ky_tab_factory, ky_spoof_factory } from '@/util/useAddon';
+import { ky as ky2, ky_tab_factory, ky_spoof_factory } from '@/util/useAddon';
 
-const ky = ky_spoof_factory('https://dict.youdao.com/');
-// const ky = ky_tab_factory('https://dict.youdao.com/');
+// const ky = ky2;
+// const ky = ky_spoof_factory('https://dict.youdao.com/');
+const ky = ky_tab_factory('https://dict.youdao.com/');
 
 const getBaseBody = (key: string) => {
   const c = 'fanyideskweb';
