b5a556c7b5641d487f944f8bf27deb68e64accca
KurikoMoe <kurikomoe@gmail.com>
2025-11-13 14:58:07 +0800

    refactor

--- web/src/util/useUserData/api.ts
+++ web/src/util/useUserData/api.ts
@@ -1,22 +1,27 @@
 import ky from 'ky';
 
 export const AuthUrl = (() => {
-  const url = new URL(window.location.href);
-  const host = url.host;
-  const protocol = url.protocol;
-  const parts = host.split('.');
-  if (parts.length > 2) {
-    // e.g. n.novelia.cc
-    return `${protocol}//auth.${parts.slice(1).join('.')}`;
-  } else if (parts.length == 2) {
-    // e.g novelia.com
-    return `${protocol}//auth.${host}`;
-  } else if (host === 'localhost') {
-    // NOTE(kuriko): For auth developing, please run auth server at port 4000
-    return `${protocol}//localhost:4000`;
-  } else {
-    throw new Error('Unsupported hostname format');
+  const { protocol, host, port } = window.location;
+  console.log('AuthUrl calculation:', { protocol, host, port });
+
+  if (host.startsWith('localhost:')) {
+    if (port === '5173') {
+      // NOTE(kuriko): pnpm dev 情况下，Auth 转发到 n.novelia.cc
+      return 'https://n.novelia.cc';
+    } else {
+      // NOTE(kuriko): 本地开发情况，Caddy 代理其他端口，转发到 5173
+      //   此时默认 Auth 服务部署在 4000 端口
+      return `${protocol}//localhost:4000`;
+    }
   }
+
+  // ALERT(kuriko): 这里不考虑 a.co.uk 这种顶级域名。
+  //  books.fishhawk.top => auth.fishhawk.top
+  //  n.novelia.cc => auth.novelia.cc
+  //  test.com => auth.test.com
+  const parts = host.split('.');
+  const baseDomain = parts.length > 2 ? parts.slice(-2).join('.') : host;
+  return `${protocol}//auth.${baseDomain}`;
 })();
 
 const client = ky.create({
